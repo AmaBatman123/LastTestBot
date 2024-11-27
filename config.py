854152e782cfc1d822fac527c85af860fd3c115b
Valerii <amabatman.12@gmail.com>
2024-11-28 00:51:55 +0600

    Added config file

---
+++ config.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher
+from decouple import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+Admins = [372040467]
+
+token = config('TOKEN')
+bot = Bot(token=token)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+async def is_admin(user_id: int) -> bool:
+    return user_id in Admins
